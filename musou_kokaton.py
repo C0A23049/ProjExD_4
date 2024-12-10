6cdd2e235df6e16e3749f7736568643283c4569b
M_shiojima <c0a230775e@edu.teu.ac.jp>
2024-12-10 18:25:36 +0900

    feature3

--- musou_kokaton.py
+++ musou_kokaton.py
@@ -8,9 +8,6 @@ import pygame as pg
 
 WIDTH = 1100  # ゲームウィンドウの幅
 HEIGHT = 650  # ゲームウィンドウの高さ
-
-
-
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -70,7 +67,7 @@ class Bird(pg.sprite.Sprite):
             (0, +1): pg.transform.rotozoom(img, -90, 0.9),  # 下
             (+1, +1): pg.transform.rotozoom(img, -45, 0.9),  # 右下
         }
-        self.dire = (+1, 0) # こうかとんの向き
+        self.dire = (+1, 0)
         self.image = self.imgs[self.dire]
         self.rect = self.image.get_rect()
         self.rect.center = xy
@@ -141,26 +138,25 @@ class Bomb(pg.sprite.Sprite):
             self.kill()
 
 
-class Beam(pg.sprite.Sprite): # ビーム
+class Beam(pg.sprite.Sprite):
     """
     ビームに関するクラス
     """
-    def __init__(self, bird: Bird, angle0=0):
+    def __init__(self, bird: Bird):
         """
         ビーム画像Surfaceを生成する
         引数 bird：ビームを放つこうかとん
         """
         super().__init__()
-        self.vx, self.vy = bird.dire # ビームの方向ベクトル
-        angle = math.degrees(math.atan2(-self.vy, self.vx)) + angle0 # ビームの角度
+        self.vx, self.vy = bird.dire
+        angle = math.degrees(math.atan2(-self.vy, self.vx))
         self.image = pg.transform.rotozoom(pg.image.load(f"fig/beam.png"), angle, 1.0)
-        self.vx = math.cos(math.radians(angle)) 
+        self.vx = math.cos(math.radians(angle))
         self.vy = -math.sin(math.radians(angle))
         self.rect = self.image.get_rect()
         self.rect.centery = bird.rect.centery+bird.rect.height*self.vy
         self.rect.centerx = bird.rect.centerx+bird.rect.width*self.vx
         self.speed = 10
-
 
     def update(self):
         """
@@ -247,60 +243,6 @@ class Score:
         self.image = self.font.render(f"Score: {self.value}", 0, self.color)
         screen.blit(self.image, self.rect)
 
-# 弾幕
-# • 設定：一度に複数方向へビームを発射する
-# • 発動条件：左Shiftキーを押下しながらスペースキー
-# • 消費スコア：なし
-# • 実装例発動条件の押下キーは自由だが必ずグループで相談すること‐50°  +50°
-# • Beamクラスのイニシャライザの引数に回転角度angle0（デフォルトで0）を追加し，ビームの回転角度に加算する
-# • NeoBeamクラスのイニシャライザの引数を，こうかとんbirdとビーム数numとする
-# • NeoBeamクラスのgen_beamsメソッドで，‐50°～+51°の角度の範囲で指定ビーム数の分だけBeamインスタンスを生成し，リストにappendする → リストを返す
-# • 発動条件が満たされたら，NeoBeamクラスのイニシャライザにこうかとんとビーム数を渡し，戻り値のリストをBeamグループに追加する
-
-# class NeoBeam:
-#     """
-#     弾幕に関するクラス
-#     """
-#     def __init__(self, bird: Bird, num: int):
-#         """
-#         弾幕ビームを生成する
-#         引数1 bird：ビームを放つこうかとん
-#         引数2 num：ビーム数
-#         """
-#         super().__init__()
-#         self.beams = self.gen_beams(bird, num) # ビームリスト
-
-#     def gen_beams(self, bird: Bird, num: int) -> list[Beam]:
-#         """
-#         弾幕ビームを生成する
-#         引数1 bird：ビームを放つこうかとん
-#         引数2 num：ビーム数
-#         戻り値：ビームリスト
-#         """
-#         beams = []
-        
-#         for i in range(-50, 51, 100//num):
-#             # ビーム数分だけビームを生成
-#             beams.append(Beam(bird, i))
-#         return beams 
-    
-#     def update(self):
-#         """
-#         弾幕ビームを更新する
-#         """
-#         for beam in self.beams:
-#             beam.update()
-class NeoBeam:
-    def __init__(self, bird: pg.sprite.Sprite, num: float):
-        self.bird = bird
-        self.num = num
-
-    def gen_beams(self) -> list[Beam]:
-        step = 100 // (self.num - 1) if self.num > 1 else 0
-        angles = range(-50, 51, step)
-        beams = [Beam(self.bird, angle) for angle in angles]
-        return beams
-
 
 class EMP(pg.sprite.Sprite):
     """
@@ -341,20 +283,14 @@ def main():
 
 
     tmr = 0
-
-
     clock = pg.time.Clock()
     while True:
         key_lst = pg.key.get_pressed()
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return 0
-            if event.type == pg.KEYDOWN and event.key == pg.K_SPACE: # スペースキーを押下
-                if key_lst[pg.K_LSHIFT]: # 左Shiftキーを押下しながらスペースキー
-                    Neo_beam = NeoBeam(bird, 5)
-                    beams.add(Neo_beam.gen_beams()) # 弾幕ビームを追加
-                else:
-                    beams.add(Beam(bird))
+            if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
+                beams.add(Beam(bird))
             if event.type == pg.KEYDOWN and event.key == pg.K_e and score.value >= 20:  # Eキー押下かつ20点以上あるなら
                 score.value -= 20  # 20点消費
                 emp.add(EMP(bombs, emys, screen))
